2bffe45369fc16709e61d8ba3851d7c003b89875
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-01-18 22:04:57 -0600

    bus + taxi refinements, preparing for sprint

--- Packages/qb-busjob/Server/Index.lua
+++ Packages/qb-busjob/Server/Index.lua
@@ -1,3 +1,32 @@
-Events.SubscribeRemote('qb-busjob:server:spawnBus', function()
-    HSimpleVehicle(Vector(21524.4, 99178.0, 102.4), Rotator(0.19579508900642, -88.273223876953, 0.13650098443031), 'abcca-qbcore-vehicles::BP_Bus')
+local peds = {}
+for i = 1, #Config.JobLocations do
+    local location_info = Config.JobLocations[i]
+    local coords = location_info.coords
+    local heading = location_info.heading
+    local ped = HCharacter(coords, Rotator(0, heading, 0), '/CharacterCreator/CharacterAssets/Avatar_FBX/Body/Male/Mesh/Male_Full_Body')
+    ped:AddSkeletalMeshAttached('head', 'helix::SK_Male_Head')
+    ped:AddSkeletalMeshAttached('chest', 'helix::SK_Man_Outwear_03')
+    ped:AddSkeletalMeshAttached('legs', 'helix::SK_Man_Pants_05')
+    ped:AddSkeletalMeshAttached('feet', 'helix::SK_Delivery_Shoes')
+
+    peds[ped] = {
+        options = {
+            {
+                type = 'client',
+                event = 'qb-busjob:client:start',
+                label = 'Start Working',
+                icon = 'fas fa-bus',
+                job = 'bus'
+            },
+        },
+        distance = 400,
+    }
+end
+
+QBCore.Functions.CreateCallback('qb-busjob:server:getPeds', function(_, cb)
+    cb(peds)
+end)
+
+Events.SubscribeRemote('qb-busjob:server:spawnBus', function(source)
+    QBCore.Functions.CreateVehicle(source, 'bp_bus', Vector(21524, 99178, 102), Rotator(0, -88, 0))
 end)
--- Packages/qb-taxijob/Server/Index.lua
+++ Packages/qb-taxijob/Server/Index.lua
@@ -1,3 +1,32 @@
-Events.SubscribeRemote('qb-busjob:server:spawnTaxi', function()
-    HSimpleVehicle(Vector(22689.3, 100016.9, 98.5), Rotator(-0.24469050765038, -90.378021240234, 0.03790046274662), 'abcca-qbcore-vehicles::BP_Taxi')
+local peds = {}
+for i = 1, #Config.JobLocations do
+    local location_info = Config.JobLocations[i]
+    local coords = location_info.coords
+    local heading = location_info.heading
+    local ped = HCharacter(coords, Rotator(0, heading, 0), '/CharacterCreator/CharacterAssets/Avatar_FBX/Body/Male/Mesh/Male_Full_Body')
+    ped:AddSkeletalMeshAttached('head', 'helix::SK_Male_Head')
+    ped:AddSkeletalMeshAttached('chest', 'helix::SK_Man_Outwear_03')
+    ped:AddSkeletalMeshAttached('legs', 'helix::SK_Man_Pants_05')
+    ped:AddSkeletalMeshAttached('feet', 'helix::SK_Delivery_Shoes')
+
+    peds[ped] = {
+        options = {
+            {
+                type = 'client',
+                event = 'qb-taxijob:client:start',
+                label = 'Start Working',
+                icon = 'fas fa-taxi',
+                job = 'taxi'
+            },
+        },
+        distance = 400,
+    }
+end
+
+QBCore.Functions.CreateCallback('qb-taxijob:server:getPeds', function(_, cb)
+    cb(peds)
+end)
+
+Events.SubscribeRemote('qb-taxijob:server:spawnTaxi', function(source)
+    QBCore.Functions.CreateVehicle(source, 'bp_taxi', Vector(22689, 100016, 98), Rotator(0, -90, 0))
 end)
