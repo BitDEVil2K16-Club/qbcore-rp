4f09053254e15550dae77f02db99c62d97f5a9fb
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-01-24 09:47:48 -0600

    Don't calback too early

--- Packages/qb-ambulancejob/Client/Index.lua
+++ Packages/qb-ambulancejob/Client/Index.lua
@@ -8,7 +8,6 @@ Events.SubscribeRemote('QBCore:Client:OnPlayerLoaded', function()
             AddTargetEntity(ped, { options = data.options, distance = data.distance })
         end
     end)
-
     Events.CallRemote('qb-ambulancejob:server:syncInjuries', Config.Bones, BleedAmount > 0 and true or false)
 end)
 
@@ -21,28 +20,28 @@ Events.Subscribe('qb-ambulancejob:client:checkStatus', function()
         if not injuryData then return end
         local statusMenu = ContextMenu.new()
         local tableOrder = { -- Not a fan of this method of sorting, but looks nice in the menu
-            [1] = "head",
-            [2] = "neck_02",
-            [3] = "neck_01",
-            [4] = "clavicle_l",
-            [5] = "clavicle_r",
-            [6] = "upperarm_l",
-            [7] = "upperarm_r",
-            [8] = "lowerarm_l",
-            [9] = "lowerarm_r",
-            [10] = "hand_l",
-            [11] = "hand_r",
-            [12] = "spine_05",
-            [13] = "spine_04",
-            [14] = "spine_03",
-            [15] = "spine_02",
-            [16] = "pelvis",
-            [17] = "thigh_l",
-            [18] = "thigh_r",
-            [19] = "calf_l",
-            [20] = "calf_r",
-            [21] = "foot_l",
-            [22] = "foot_r",
+            [1] = 'head',
+            [2] = 'neck_02',
+            [3] = 'neck_01',
+            [4] = 'clavicle_l',
+            [5] = 'clavicle_r',
+            [6] = 'upperarm_l',
+            [7] = 'upperarm_r',
+            [8] = 'lowerarm_l',
+            [9] = 'lowerarm_r',
+            [10] = 'hand_l',
+            [11] = 'hand_r',
+            [12] = 'spine_05',
+            [13] = 'spine_04',
+            [14] = 'spine_03',
+            [15] = 'spine_02',
+            [16] = 'pelvis',
+            [17] = 'thigh_l',
+            [18] = 'thigh_r',
+            [19] = 'calf_l',
+            [20] = 'calf_r',
+            [21] = 'foot_l',
+            [22] = 'foot_r',
         }
         for k, v in pairs(tableOrder) do
             statusMenu:addButton(k, string.format('%s | Damaged: %s | Severity: %s', injuryData.limbs[v].label, injuryData.limbs[v].isDamaged and 'Yes' or 'No', injuryData.limbs[v].severity))
@@ -103,17 +102,11 @@ AddGlobalPlayer({
     distance = 800,
 })
 
-QBCore.Functions.TriggerCallback('qb-ambulancejob:server:getPeds', function(peds)
-    for ped, data in pairs(peds) do
-        AddTargetEntity(ped, { options = data.options, distance = data.distance })
-    end
-end)
-
 for _, v in pairs(Config.Locations.hospital) do
     local location = v.location
     Events.Call('Map:AddBlip', {
         name = 'Hospital',
-        coords = {x = location.X, y = location.Y, z = location.Z},
+        coords = { x = location.X, y = location.Y, z = location.Z },
         imgUrl = './media/map-icons/Medicine-icon.svg'
     })
 end
