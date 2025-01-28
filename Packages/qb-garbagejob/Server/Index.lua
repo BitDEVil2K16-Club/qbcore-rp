01c10146af3489d59a268f78451b0cc9e0199548
Joel-Hypersonic <joel@hypersoniclabs.co>
2025-01-28 14:53:03 +0000

    feat: implement peds with depot locations

--- Packages/qb-garbagejob/Server/Index.lua
+++ Packages/qb-garbagejob/Server/Index.lua
@@ -1,1 +1,24 @@
 local Lang = Package.Require('../Shared/locales/' .. QBConfig.Language .. '.lua')
+local peds = {}
+
+for _, depot in pairs(Config.Locations.Depots) do
+    local pedData = depot.pedSpawn
+    local ped = HCharacter(pedData.coords, Rotator(0, pedData.heading, 0), '/CharacterCreator/CharacterAssets/Avatar_FBX/Body/Male/Mesh/Male_Full_Body')
+    ped:AddSkeletalMeshAttached('head', 'helix::SK_Male_Head')
+    ped:AddSkeletalMeshAttached('chest', 'helix::SK_Man_Outwear_03')
+    ped:AddSkeletalMeshAttached('legs', 'helix::SK_Man_Pants_05')
+    ped:AddSkeletalMeshAttached('feet', 'helix::SK_Delivery_Shoes')
+
+    peds[ped] = {
+        options = {
+            {
+                type = 'server',
+                event = 'QBCore:ToggleDuty',
+                label = 'Toggle Duty',
+                icon = 'fas fa-clipboard',
+                jobType = 'garbage'
+            },
+        },
+        distance = 400,
+    }
+end
