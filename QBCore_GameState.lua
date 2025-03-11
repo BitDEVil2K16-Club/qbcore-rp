06c7658d0666bb22de4e6ecda5af6e00850507cf
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-03-11 10:22:52 -0500

    Remove unneeded code

--- QBCore_GameState.lua
+++ QBCore_GameState.lua
@@ -1,11 +1,8 @@
 ---@type QBCore_GameState_C
 local M = UnLua.Class()
 
-function M:ReceiveBeginPlay()
-    local QBCore = require '/QBCore/qb-core/core':GetCoreObject()
-    UE.UGameplayStatics.GetGameState(self).QBCore = QBCore
-    _G['QBCore'] = QBCore
-end
+-- function M:ReceiveBeginPlay()
+-- end
 
 -- function M:ReceiveEndPlay()
 -- end
