e5458f491525dcaf7b841ce8e8e882f3c048dccb
Kakarot <57848836+GhzGarage@users.noreply.github.com>
2025-10-27 13:59:19 -0500

    update vehicles

--- qb-core/Shared/vehicles.lua
+++ qb-core/Shared/vehicles.lua
@@ -1,14 +1,26 @@
 QBCore.Shared.Vehicles = {
+	-- Advanced Vehicles
 	bp_police = {
-		asset_name = 'helix::BP_PoliceCar',
+		asset_name = '/Game/SimpleVehicle/Blueprints/BP_PoliceCar.BP_PoliceCar_C',
 		collision_type = CollisionType.Normal,
 		gravity_enabled = true,
 	},
-	bp_simcade = {
-		asset_name = 'helix::BP_Simcade',
+	bp_axiump = {
+		asset_name = '/VehicleSystem/Blueprints/Vehicles/AxiumP/BP_AxiumP_Vehicle.BP_AxiumP_Vehicle_C',
 		collision_type = CollisionType.Normal,
 		gravity_enabled = true,
 	},
+	bp_bauhause = {
+		asset_name = '/VehicleSystem/Blueprints/Vehicles/Bauhause/BP_Bauhause_Vehicle.BP_Bauhause_Vehicle_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_medusa = {
+		asset_name = '/VehicleSystem/Blueprints/Vehicles/Medusa/BP_Medusa_Vehicle.BP_Medusa_Vehicle_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	-- Job Vehicles
 	bp_ambulance = {
 		asset_name = '/abcca-qbcore-veh/QBCoreVehicles/BP_Ambulance.BP_Ambulance_C',
 		collision_type = CollisionType.Normal,
@@ -71,17 +83,7 @@ QBCore.Shared.Vehicles = {
 		gravity_enabled = true,
 	},
 	bp_pongasera = {
-		asset_name = '/abcca-dax-veh/PongaseraGt/Blueprint/BP_PongaseraGtVehicle.BP_PongaseraGtVehicle_C',
-		collision_type = CollisionType.Normal,
-		gravity_enabled = true,
-	},
-	bp_swatvan = {
-		asset_name = '/abcca-dax-veh/SWATVan/Blueprint/BP_SwatVan.BP_SwatVan_C',
-		collision_type = CollisionType.Normal,
-		gravity_enabled = true,
-	},
-	bp_serenity = {
-		asset_name = '/abcca-dax-veh/Serenity/Blueprint/BP_SerenityVehicle.BP_SerenityVehicle_C',
+		asset_name = '/abcca-dax-veh/PongaseraGt/Blueprint/BP_PongaseraGt.BP_PongaseraGt_C',
 		collision_type = CollisionType.Normal,
 		gravity_enabled = true,
 	},
@@ -90,13 +92,116 @@ QBCore.Shared.Vehicles = {
 		collision_type = CollisionType.Normal,
 		gravity_enabled = true,
 	},
-	bp_volt = {
-		asset_name = '/abcca-dax-veh/Volt/Blueprint/BP_VoltVehicle.BP_VoltVehicle_C',
+	bp_serenity = {
+		asset_name = '/abcca-dax-veh/Serenity/Blueprint/BP_SerenityVehicle.BP_SerenityVehicle_C',
 		collision_type = CollisionType.Normal,
 		gravity_enabled = true,
 	},
 	bp_siesta = {
 		asset_name = '/abcca-dax-veh/SiestaSedan/Blueprint/BP_SiestaVehicle.BP_SiestaVehicle_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_swatvan = {
+		asset_name = '/abcca-dax-veh/SWATVan/Blueprint/BP_SWATVan.BP_SWATVan_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_volt = {
+		asset_name = '/abcca-dax-veh/Volt/Blueprint/BP_VoltVehicle.BP_VoltVehicle_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	-- Tanks
+	bp_panzer = {
+		asset_name = '/Game/ModularTanks/BegleitPanzer/BP_BegleitPanzer.BP_BegleitPanzer_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_bmp2 = {
+		asset_name = '/Game/ModularTanks/BMP2/BP_BMP2.BP_BMP2_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_ariete = {
+		asset_name = '/Game/ModularTanks/C1_Ariete/BP_C1_Ariete.BP_C1_Ariete_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_challenger1 = {
+		asset_name = '/Game/ModularTanks/Challenger1/BP_Challenger1.BP_Challenger1_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_challenger2 = {
+		asset_name = '/Game/ModularTanks/Challenger2/BP_Challenger2.BP_Challenger2_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_chieftain = {
+		asset_name = '/Game/ModularTanks/ChieftainMK6/BP_ChieftainMK6.BP_ChieftainMK6_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_leopard = {
+		asset_name = '/Game/ModularTanks/Leopard2A7/BP_Leopard2A7.BP_Leopard2A7_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_abrahams = {
+		asset_name = '/Game/ModularTanks/M1Abrahams/BP_M1Abrahams.BP_M1Abrahams_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_of40 = {
+		asset_name = '/Game/ModularTanks/OF40/BP_OF40.BP_OF40_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_h2000 = {
+		asset_name = '/Game/ModularTanks/PzH2000/BP_PzH2000.BP_PzH2000_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_t64 = {
+		asset_name = '/Game/ModularTanks/T64/BP_T64.BP_T64_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_t90 = {
+		asset_name = '/Game/ModularTanks/T90/BP_T90.BP_T90_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	-- Choppers
+	bp_aheli = {
+		asset_name = '/Game/Helicopter_customizable/Blueprints/BP_Heli_Ambulance.BP_Heli_Ambulance_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_mheli = {
+		asset_name = '/Game/Helicopter_customizable/Blueprints/BP_Heli_Military.BP_Heli_Military_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_pheli = {
+		asset_name = '/Game/Helicopter_customizable/Blueprints/BP_Heli_Police.BP_Heli_Police_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_heli = {
+		asset_name = '/Game/Helicopter_customizable/Blueprints/BP_Helicopter.BP_Helicopter_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	-- Planes
+	bp_rafale = {
+		asset_name = '/Game/VigilanteMovement/Blueprints/BP_Rafale.BP_Rafale_C',
+		collision_type = CollisionType.Normal,
+		gravity_enabled = true,
+	},
+	bp_rc135 = {
+		asset_name = '/Game/VigilanteMovement/Blueprints/BP_RC135.BP_RC135_C',
 		collision_type = CollisionType.Normal,
 		gravity_enabled = true,
 	},
